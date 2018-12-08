5adfd4aab57fc06d04547779dc93d573808e4c6c
ChaeLinYeo <evergreen786@naver.com>
2018-12-08 17:17:58 +0900

    Update admin.py

--- hotelsite/staff/admin.py
+++ hotelsite/staff/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-from .models import Room, Request_post, Department, Staff
+from .models import Room, Request_post, Department, Staff, Reservation, Type, Extracost, Guest, Membership, Guestcar, Charge
 
 admin.site.unregister(User)
 
@@ -23,3 +23,11 @@ admin.site.register(User, ProfileAdmin)
 admin.site.register(Room)
 admin.site.register(Request_post) 
 admin.site.register(Department)
+admin.site.register(Reservation)
+admin.site.register(Staff)
+admin.site.register(Type)
+admin.site.register(Extracost)
+admin.site.register(Guest)
+admin.site.register(Membership)
+admin.site.register(Guestcar)
+admin.site.register(Charge)
