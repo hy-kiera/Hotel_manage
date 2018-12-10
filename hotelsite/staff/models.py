e5ef3fb30caa854c2e353cff7ef56d2ed7380c16
kjy11 <44992788+kjy11@users.noreply.github.com>
2018-12-10 21:55:22 +0900

    staff모델에 사진추가 (#64)

--- hotelsite/staff/models.py
+++ hotelsite/staff/models.py
@@ -2,29 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from login.models import Guest
+from django.urls import reverse_lazy
+
+def user_path(instance, filename): #파라미터 instance는 Photo 모델을 의미 filename은 업로드 된 파일의 파일 이름
+    from random import choice
+    import string # string.ascii_letters : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr) # 8자리 임의의 문자를 만들어 파일명으로 지정
+    extension = filename.split('.')[-1] # 배열로 만들어 마지막 요소를 추출하여 파일확장자로 지정
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<random>
+    return '%s/%s.%s' % (instance.owner.username, pid, extension) # 예 : wayhome/abcdefgs.png
 
 class Room(models.Model): 
- 
+
     STATUS_CLEAN_IN_CHOICES=(
         ('CLEAN', 'Clean'),
         ('UNCLEAN', 'Unclean')
     )
+
     room_num = models.DecimalField(decimal_places=0, max_digits=4, primary_key='True') 
     room_type = models.ForeignKey('Type', on_delete=models.CASCADE) #Suite 
     status_clean = models.CharField(max_length=10, choices=STATUS_CLEAN_IN_CHOICES, default='CLEAN')  # 0 : 사용중, 1 : 사용가능, 3 : 청소 필요, 4 : 예약
     room_floor = models.DecimalField(decimal_places=0, max_digits=1, default='1')
-    
-    
+
     def __str__(self):
         return str(self.room_num)+'호'
 
 class Staff(models.Model):
-
     SEX_IN_CHOICES=(
         ('MALE', 'male'),
         ('FEMALE', 'female')
     )
-
     STATUS_IN_CHOICES=(
         ('WORKING', 'working'),
         ('RESTING', 'resting'),
@@ -43,14 +51,17 @@ class Staff(models.Model):
     sex = models.CharField(max_length=6, choices=SEX_IN_CHOICES)
     status = models.CharField(max_length=7, choices=STATUS_IN_CHOICES)
     phone_num = models.CharField(max_length=13) # 010-0000-0000
-    # photo = models.ImageField()
+    photo = models.ImageField(upload_to='images')
     dept = models.ForeignKey('Department', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.user.username
 
+    def get_absolute_url(self):
+        url = reverse_lazy('detail', kwargs={'pk': self.pk})
+        return url
+
 class Department(models.Model):
-  
     DEPT_IN_CHOICES=(
         ('BACK_OFFICE', 'back_office'),
         ('SALES_MARKETING', 'sales_marketing'),
@@ -60,7 +71,6 @@ class Department(models.Model):
         ('FOOD_BERVERAGE', 'food_berverage'),
         ('FINANCE', 'finance'),
     )
-    
     name = models.CharField(max_length = 30, choices=DEPT_IN_CHOICES, unique=True) 
     role = models.TextField() 
     tel_num = models.CharField(max_length = 20) 
@@ -73,7 +83,6 @@ class Request_post(models.Model):
     UNASSIGNED = 1
     NOT_YET=2
     DONE=3
-
     HANDLE_IN_CHOICES=(
         (UNASSIGNED, '미배정'),
         (NOT_YET, '처리중'),
